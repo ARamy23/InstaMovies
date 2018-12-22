fb394917df2a886430b30d346785c4cd15c8cac2
Ahmed Ramy <Dev.AhmedRamy@gmail.com>
2018-12-22 19:00:28 +0200

    add documentation to dynamic class

--- InstaMovies/Shared Components/Extensions/Dynamic.swift
+++ InstaMovies/Shared Components/Extensions/Dynamic.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+/// This is a simple class which allows binding without actually using a 3rd party, it is used for just
+/// one simple use case which is, u are binding a property in a layer with another on the same thread in
+/// a one way, meaning that u just come to one layer and bind it to the other (i prefer the controller to the viewModel)
+/// however, two way binding and binding while the two properties are not on the same thread, would break the app
+/// how would it break the app?
+/// two way binding would make an infinite loop of each property in both layers are setting each other one after another
+/// if we used bind from a thread, then we are actually... well, dealing with bits and bytes being modified, so u get a really unexpected results
 class Dynamic<T> {
     
     var bind :(T) -> () = { _ in }
